2cc9353da884c5e99c72ad313b715f3c545b387d
DavidLBruno <bruno.david9914@gmail.com>
2023-06-23 19:43:20 -0300

    bug bash

--- generar.sh
+++ generar.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
-
 	
-for i in {seq  1 $1}
-
-do	
-	nombre=$(sed -n"$RANDOM"p array)	
-	echo $nombre
-	curl -L  https://source.unsplash.com/random/900%C3%97700/?person --output $nombre
+for i in {1..$1}
+do
+        random=$(($RANDOM/100))
+	nombre=$(sed -n "$random"p array)
+	curl -L https://source.unsplash.com/random/900%C3%97700/?person --output $nombre
 	sleep 1
 done
