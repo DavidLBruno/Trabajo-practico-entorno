06fb532e7ab71e84a8d2d29376365aee14c51031
DavidLBruno <bruno.david9914@gmail.com>
2023-06-23 20:54:33 -0300

    seccion 1

--- generar.sh
+++ generar.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-	
-for i in {1..$1}
+
+
+# Download image with a for
+mkdir images
+for i in $(seq 1 "$1")
 do
-        random=$(($RANDOM/100))
-	nombre=$(sed -n "$random"p array)
-	curl -L https://source.unsplash.com/random/900%C3%97700/?person --output $nombre
+	nombre=$(sed -n "$RANDOM"p array)
+	cd images
+	curl -L https://source.unsplash.com/random/900%C3%97700/?person --output "$nombre"
+	cd ..
 	sleep 1
 done
+
+cd images
+zip ../images *
+cd ..
+
+hash=$(md5sum images.zip)
+
+touch images.zip.sum
+
+echo $hash > images.zip.sum
+
+echo Todo salio correctamente
