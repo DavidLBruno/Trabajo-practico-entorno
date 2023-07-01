52660dbda62be133041c3fb0bb2c478e886a9759
alumno <alumno>
2023-06-30 21:59:31 -0300

    bugs

--- generar.sh
+++ generar.sh
@@ -3,10 +3,18 @@
 
 # Download image with a for
 mkdir images
-curl -L https://raw.githubusercontent.com/adalessandro/EdP-2023-TP-Final/main/dict.csv array.txt
+
+if [[ ! -e array.txt ]]
+then
+curl -L https://raw.githubusercontent.com/adalessandro/EdP-2023-TP-Final/main/dict.csv --output array.txt
+fi
+
+lines=$(wc -l < ./array.txt)
+
 for i in $(seq 1 "$1")
 do
-	nombre=$(sed -n "$RANDOM"p array.txt)
+	nombre=$(sed -n "$RANDOM%$lines+1"p array.txt)
+  echo $nombre AAAAAA
 	cd images
 	curl -L https://source.unsplash.com/random/900%C3%97700/?person --output "$(echo $nombre | cut -d , -f 1)"
 	cd ..
