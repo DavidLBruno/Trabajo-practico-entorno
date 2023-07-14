9c2fd1c50b53c81caa6247aaff48082f0aaf3037
Mateo <mateomansillakohen@outlook.com>
2023-07-14 00:19:04 -0300

    procesar.sh

--- procesar.sh
+++ procesar.sh
@@ -1,45 +1,24 @@
 #!/bin/bash
 
-
-
 directorio="images"
-
-
 
 cd "$directorio" || exit 1
 
-
-
 directorio_destino="../imagenesRecortadas"
-
 mkdir -p "$directorio_destino"
 
-
-
 for archivo in *; do
-
     if [[ -f "$archivo" ]]; then
-
         nombre_archivo=$(basename "$archivo")
-
         if [[ $nombre_archivo =~ ^[A-Z][a-z]+([[:space:]][A-Z][a-z]+)$ ]]; then
-
             echo "Nombre Verificado: $nombre_archivo"
-
             archivo_destino="$directorio_destino/$nombre_archivo"
-
             convert "$archivo" -resize 512x512^ -gravity center -extent 512x512 "$archivo_destino"
-
             echo "Imagen recortada: $archivo_destino"
-
         else
-
             echo "Nombre Inválido: $nombre_archivo"
-
             exit 1
-
         fi
-
     fi
-
 done
+
