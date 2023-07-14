308be570abf08eff7b6189e05988c6b890df770b
Bruno David <93402677+DavidLBruno@users.noreply.github.com>
2023-07-14 01:00:11 -0300

    decomprimir finalizado

--- descomprimir.sh
+++ descomprimir.sh
@@ -3,26 +3,38 @@
 archivo=$1
 validacion=$2
 
-echo $archivo $validacion
-
 if [[ -z $archivo || -z $validacion ]]
 then
-  echo Debe colocar un archivo comprimido y una suma de veirificacion.
-  exit 0
-fi
-
-if [[ $archivo && $validacion ]]
+	echo Debe colocar al menos dos valores.
+	exit 0
+elif [[ ! -e $archivo ]]
 then
- echo $archivo debe ser un archivo comprimido.
-  exit 0
-fi
-
-if [[ ]]
-  echo $validacion debe ser una suma de verificacion.
-  exit 0
+	echo $archivo Debe existir.
+	exit 0
+elif [[ ! -e $validacion ]]
 then
+	echo $validacion Debe existir.
+	exit 0
+elif [[ $archivo != *.zip ]]
+then
+        echo $archivo debe ser un archivo comprimido.
+        exit 0
+elif [[ $validacion != *.zip.sum ]]
+then
+        echo $validacion debe ser una suma de verificacion.
+	exit 0
 fi
 
+suma_verificacion=$(cat $validacion | awk '{ print $1 }')
+hash=$(md5sum images.zip | awk '{ print $1 }')
+
+if [[ "$suma_verificacion" != "$hash" ]]
+then
+	echo La suma de verificacion no coinciden.
+	exit 0
+fi
+
+unzip $archivo -d ./imagenes-descomprimidas
 echo Archivo descomprimido con exito!
 
 exit 0
